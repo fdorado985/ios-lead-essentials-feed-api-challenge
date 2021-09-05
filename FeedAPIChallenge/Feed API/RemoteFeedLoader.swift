71fe3f3f659687a2bdc98969b1b92351f2f1793f
Juan Dorado <fdorado985@gmail.com>
2021-09-04 21:11:39 -0500

    Load does not deliver result after SUT instance has been deallocated

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,7 +19,8 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { result in
+		client.get(from: url) { [weak self] result in
+			if self == nil { return }
 			switch result {
 			case let .success((data, response)):
 				guard response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
