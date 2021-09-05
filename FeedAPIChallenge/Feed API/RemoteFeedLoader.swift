f63620a019958fef4255b494fd3ced5c6c820608
Juan Dorado <fdorado985@gmail.com>
2021-09-04 21:19:17 -0500

    Move the Network models inside the `FeedImagesMapper`

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -37,6 +37,28 @@ public final class RemoteFeedLoader: FeedLoader {
 }
 
 private class FeedImagesMapper {
+	private struct Root: Decodable {
+		let items: [Item]
+	}
+
+	private struct Item: Decodable {
+		let id: UUID
+		let description: String?
+		let location: String?
+		let url: URL
+
+		var item: FeedImage {
+			FeedImage(id: id, description: description, location: location, url: url)
+		}
+
+		private enum CodingKeys: String, CodingKey {
+			case id = "image_id"
+			case description = "image_desc"
+			case location = "image_loc"
+			case url = "image_url"
+		}
+	}
+
 	static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedImage] {
 		guard response.statusCode == 200 else {
 			throw RemoteFeedLoader.Error.invalidData
@@ -44,27 +66,5 @@ private class FeedImagesMapper {
 
 		let root = try JSONDecoder().decode(Root.self, from: data)
 		return root.items.map { $0.item }
-	}
-}
-
-private struct Root: Decodable {
-	let items: [Item]
-}
-
-private struct Item: Decodable {
-	let id: UUID
-	let description: String?
-	let location: String?
-	let url: URL
-
-	var item: FeedImage {
-		FeedImage(id: id, description: description, location: location, url: url)
-	}
-
-	private enum CodingKeys: String, CodingKey {
-		case id = "image_id"
-		case description = "image_desc"
-		case location = "image_loc"
-		case url = "image_url"
 	}
 }
