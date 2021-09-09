6b6eaa7004899c895f005922a880ab6ad5c0a513
Juan Dorado <fdorado985@gmail.com>
2021-09-08 19:31:15 -0500

    Remove the `wildcard` for `.failure` type, because it is not used anyways

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -24,7 +24,7 @@ public final class RemoteFeedLoader: FeedLoader {
 			switch result {
 			case let .success((data, response)):
 				completion(Result { try FeedImagesMapper.map(data, response) })
-			case .failure(_):
+			case .failure:
 				completion(.failure(Error.connectivity))
 			}
 		}
